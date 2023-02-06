a5b175a3edddddc22099421d1154c0be0c78fe09
zachrking <zachkingx010@gmail.com>
2023-02-06 10:48:22 +0530

    changes done in the user side

--- user-management-backend/src/main/java/com/mclebtec/demo/model/User.java
+++ user-management-backend/src/main/java/com/mclebtec/demo/model/User.java
@@ -1,7 +1,5 @@
 package com.mclebtec.demo.model;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,19 +14,13 @@ import org.springframework.data.mongodb.core.mapping.Field;
 @AllArgsConstructor
 @Getter
 @Setter
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class User {
-
     @Id
     private ObjectId id;
-    @JsonProperty(value = "first_name", required = true)
     @Field(name = "first_name")
     private String firstName;
-    @JsonProperty(value = "last_name", required = true)
     @Field(name = "last_name")
     private String lastName;
-    @JsonProperty(value = "email_id", required = true)
     @Field(name = "email_id")
     private String emailId;
-
 }
